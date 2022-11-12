039ffe5a14570f1d85be5e30da1293f49a17d27e
Anagho Okiroro Obruthe <anaghosamroyce@gmail.com>
2022-11-12 08:45:37 +0100

    todo api task

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
 const mongoose = require("mongoose");
 const connectDB = require("./config/dbConn");
 const PORT = process.env.PORT || 3500;
 
 // Connect to MongoDB
 connectDB();
+
+// Custom middleware logger
+app.use(logger);
 
 // built-in middlewares to handle urlencoded form data
 app.use(express.urlencoded({ extended: false }));
@@ -16,6 +21,9 @@ app.use(express.json());
 
 // routes
 app.use("/todos", require("./routes/todos"));
+
+// Error handler
+app.use(errorHandler);
 
 mongoose.connection.once("open", () => {
   console.log("Connected to MongoDB");
